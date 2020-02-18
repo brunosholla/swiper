e1deadf5b63955f5f92c38a68bc81f787cce4fcc
Dea19 <dea.mulaj@gmail.com>
2020-02-18 17:51:57 +0100

    ok

--- App.js
+++ App.js
@@ -1,69 +1,146 @@
-import React from 'react';
-import {YellowBox, AppRegistry, StyleSheet, Text, View, Dimensions } from 'react-native'
+import React, {Component} from 'react';
+import {YellowBox,TouchableHighlight, Button,AppRegistry,TouchableOpacity, StyleSheet,FlatList, Text, View, Dimensions } from 'react-native'
 import Swiper from 'react-native-swiper'
 import Icon from "react-native-vector-icons/FontAwesome5";
+import {
+    responsiveHeight,
+    responsiveWidth,
+    responsiveFontSize
+} from "react-native-responsive-dimensions";
+
+
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const SCREEN_HEIGHT = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   wrapper: {
-    width:Dimensions.get("window").width/3
+    width:responsiveWidth(30)
   },
   slide1: {
+     // height:Dimensions.get("window").height,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#9DD6EB'
+    backgroundColor: '#9DD6EB',
   },
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#97CAE5'
-  },
-  slide3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9'
-  },
-  text: {
-    color: '#fff',
-    fontSize: 30,
-    fontWeight: 'bold'
-  }
+
+
+    button:{
+        marginBottom: 10
+    },
+    SubmitButtonStyle: {
+
+        marginTop:10,
+        paddingTop:15,
+        paddingBottom:15,
+        marginLeft:5,
+        marginRight:5,
+        backgroundColor:'#00BCD4',
+        borderRadius:20,
+        borderWidth: 2,
+        borderColor: '#fff'
+    },
+
+    TextStyle:{
+        color:'#fff',
+        textAlign:'center',
+    }
 })
 
 
+export default class App extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            isLoading: true,
+            data:[],
+            start: 0,
+            difference:6,
+            end: 6,
+        }
+    }
 
-export default function App() {
+    componentDidMount(){
+        return fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson)
+                this.setState({
+                    isLoading: false,
+                    data: responseJson,
+                }, function(){
 
-  return (
-      <View style={{
+                });
+
+            })
+            .catch((error) =>{
+                console.error(error);
+            });
+    }
+
+
+    render() {
+        const {data, start, end, difference} = this.state;
+        const SCREEN_WIDTH = Dimensions.get("window").width;
+        const SCREEN_HEIGHT = Dimensions.get("window").height;
+        //console.log(Math.round(responsiveFontSize(2)));
+        console.log(SCREEN_HEIGHT - 2* 0.03*SCREEN_HEIGHT);
+
+
+        return (
+            <View style={{
         flex: 1,
         flexDirection: 'column',
 
         alignItems: 'flex-start',
       }}>
-        <Swiper
-            dotStyle={{opacity:0}}
-            activeDotStyle={{opacity:0}}
-            horizontal={false }
-            style={styles.wrapper}
-            showsButtons={true}
-            prevButton={<Icon  name="chevron-up" size={20} solid />}
-            nextButton={ <Icon  name="chevron-down" size={20} solid />}
-            buttonWrapperStyle={{backgroundColor: 'transparent', flexDirection: 'column', position: 'absolute', top: 0, left: 0, flex: 1, paddingHorizontal: 10, paddingVertical: 10, justifyContent: 'space-between'}}>
-          <View style={styles.slide1}>
-            <Text style={styles.text}>Hello Swiper</Text>
-          </View>
-          <View style={styles.slide2}>
-            <Text style={styles.text}>Beautiful</Text>
-          </View>
-          <View style={styles.slide3}>
-            <Text style={styles.text}>And simple</Text>
-          </View>
-        </Swiper>
-      </View>
-  )
+                <Swiper
+                    dotStyle={{opacity:0}}
+                    activeDotStyle={{opacity:0}}
+                    horizontal={false }
+                    style={styles.wrapper}
+                    showsButtons={true}
+                    prevButton={<Icon  name="chevron-up" size={SCREEN_HEIGHT * 0.03} solid
+                    onPress={() => this.setState({start: start-difference, end: end-difference})}
+
+                    />}
+                    nextButton={ <Icon
+                     onPress={() => this.setState({start: start+difference, end: end+difference})}
+                     name="chevron-down" size={SCREEN_HEIGHT * 0.03} solid />}
+                    buttonWrapperStyle={{backgroundColor: 'transparent', flexDirection: 'column', position: 'absolute', top: 0, left: 0, flex: 1, paddingHorizontal: 10, paddingVertical: 10, justifyContent: 'space-between'}}>
+                    {data.map((item, key) => {
+
+                        return (
+                            <View key={key} style={styles.slide1}>
+                                <FlatList
+                                    contentContainerStyle={{ marginTop: 50}}
+                                    data={data.slice(start,end)}
+                                    renderItem={({ item, index }) => (
+                                    <View style={styles.button}>
+                                    <TouchableOpacity
+          style={styles.SubmitButtonStyle}
+          activeOpacity = { .5 }
+          onPress={ this.ButtonClickCheckFunction }
+       >
+
+            <Text style={styles.TextStyle}> {item.name} </Text>
+
+      </TouchableOpacity>
+                                    </View>
+                                        )}
+                                    />
+                             </View>
+                        )
+                    })}
+
+
+
+
+                </Swiper>
+            </View>
+        )
+    }
 }
 
 
